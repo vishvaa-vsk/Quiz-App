0b275c1327e4fc48c325c0157b16eb35991da532
vishvaa-vsk <kvishvaa6@gmail.com>
2023-10-09 19:41:56 +0530

    Added reset password feature for all

---
+++ ex.py
@@ -0,0 +1,11 @@
+print("""Hello {{username}},
+      
+A request has been received to change the password for your VEC Quiz app account.
+
+Reset Password
+      
+If you did not initiate this request, please contact us immediately in communicative english lab!
+      
+Thank you,
+The Communicative English Team
+""")
